8c0db9607a7214950cdb07a090f64f1bca4c76a7
Tancholpon <tancholponbakytbekkyzy@gmail.com>
2024-01-25 11:26:35 +0600

    Refactor VisaPayment class

--- src/payment/VisaPayment.java
+++ src/payment/VisaPayment.java
@@ -1,9 +1,4 @@
-package payment;
+package Payment;
 
 public class VisaPayment {
-
-    public void payByVisa (String cardHolderName, int cardNumber){
-
-        System.out.println(cardHolderName + " " + cardNumber);
-    }
 }
