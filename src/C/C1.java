d86b8601b275951b7a07c82411f469a7bc4ec54b
Tancholpon <tancholponbakytbekkyzy@gmail.com>
2024-01-26 10:50:42 +0600

    C1-commit-3

--- src/C/C1.java
+++ src/C/C1.java
@@ -4,4 +4,9 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public static void sayHello (){
+        System.out.println("Hello");
+
+    }
 }
